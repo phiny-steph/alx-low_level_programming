d5e2fdd28dddb66492f3cb20986c34da64336527
phiny-steph <anniestephanie00@gmail.com>
2022-07-25 11:21:15 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,37 +1,20 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
-* main - multiply two numbers
-* @argc: number of arguments
-* @argv: arguments entered
-* Description: return product
-* Return: zero
-*/
+ * main - multiplies two numbers followed by newline or
+ *nprint Error if program does not recieve two arguments
+ * @argc: argument count
+ * @argv: argument entered
+ * Return: 0(success) or 1 if not two arguments
+ */
 
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *s1 = argv[1];
-	char *s2 = argv[2];
-
-	num1 = num2 = 0;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (s1[0] == '-')
-		num1 = -1 * atoi(&s1[1]);
-	else
-		num1 = atoi(&s1[0]);
-	if (s2[0] == '-')
-		num2 = -1 * atoi(&s2[1]);
-	else
-		num2 = atoi(&s2[0]);
-
-	printf("%d\n", num1 * num2);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
