994e84baf7019883ce640de4e8a3bb96a43958a5
phiny-steph <anniestephanie00@gmail.com>
2022-07-25 11:45:43 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,72 +1,59 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * checkNum - verifies if string has only numbers
+ * isInteger - checks if s is an integer
  * @s: string to check
- * Description: return 1 if only numbers in string, 0 if not
- * Return: 1 if only numbers, 0 if not
- **/
+ * Return: 0 or 1
+ */
 
-int checkNum(char *s)
+int isInteger(const char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+
+	while (s[i] != '\0')
 	{
 		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
 	}
 	return (1);
 }
 
 /**
- * main - find least amount of coins needed for change
- * @argc: number of parameters entered
- * @argv: strings entered including file name
- * Description: prints the minimum number of coins to make change
- * Return: zero
- **/
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	int change, coins;
-	char *input = argv[1];
-
-	change = coins = 0;
+	int i = 0, coinUsed = 0, coin = 0;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	change = atoi(input);
-
-	if (change >= 0 && checkNum(input) == 0)
+	if (isInteger(argv[1]))
 	{
-		printf("Error\n");
-		return (1);
+		i = atoi(argv[1]);
+		while (i > 0 && coin <= 4)
+		{
+			if (i >= coins[coin])
+			{
+				i -= coins[coin];
+				coinUsed++;
+			}
+			else
+			{
+				coin++;
+			}
+		}
 	}
-	if (change < 0 && checkNum(&input[1]) == 0)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if (change < 0)
-	{
-		change = 0;
-	}
-	coins = change / 25;
-	change = change % 25;
-	coins = coins + change / 10;
-	change = change % 10;
-	coins = coins + change / 5;
-	change = change % 5;
-	coins = coins + change / 2;
-	change = change % 2;
-	coins += change;
-	printf("%d\n", coins);
+	printf("%i\n", coinUsed);
 	return (0);
 }
